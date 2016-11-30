1e7cc48475e25c9122b7e0d599ea3ec3c455bee2
Marty Eason <martye@sideways8.com>
2016-11-30 05:54:47 -0500

    Stable featured image selector for taxonomies

--- code/FeaturedImage/TaxonomyFeaturedImage.php
+++ code/FeaturedImage/TaxonomyFeaturedImage.php
@@ -30,13 +30,146 @@ class TaxonomyFeaturedImage
      */
     protected $bindings = [];
 
+    protected function _script()
+    {
+        ?>
+        <script>
+            (function($) {
+
+                $(document).ready(function() {
+
+                    // WP Media Frame.
+                    var imageContainer = $('#image-container'),
+                        imageIdInput = $('#taxonomy_featured_image'),
+                        uploadButton = $('#select_taxonomy_image'),
+                        frame = new wp.media.view.MediaFrame.Select({
+                            title: 'Select taxonomy image',
+                            multiple: false,
+                            library: {
+                                order: 'ASC',
+                                orderby: 'title',
+                                type: 'image'
+                            },
+                            button: {
+                                text: 'Set taxonomy image'
+                            }
+                        });
+
+                    // Initialize frame state.
+                    frame.state();
+                    frame.lastState();
+
+                    frame.on( 'select', function() {
+                        var selectionCollection = frame.state().get('selection'),
+                            models = selectionCollection.models,
+                            image = models[0],
+                            imageId = image.attributes.id,
+                            imageUrl = 'undefined' == typeof( image.attributes.sizes.medium ) ?
+                                image.attributes.sizes.full.url : image.attributes.sizes.medium.url;
+
+                        imageContainer.html('<img src="' + imageUrl + '">');
+                        imageIdInput.val(imageId);
+                        imageIdInput.attr('value', imageId);
+                    });
+
+                    uploadButton.on( 'click', function() {
+                        frame.open();
+                    });
+                });
+            })(jQuery);
+        </script>
+        <?php
+    }
+
+    /**
+     * HTML for add taxonomy pages.
+     */
+    protected function _addFormFields()
+    {
+        wp_enqueue_media();
+        ?>
+        <div class="form-field">
+            <label for="taxonomy_featured_image"><?php _e( 'Taxonomy Featured Image:', 'wp-orbit' ); ?></label>
+            <input type="hidden" name="taxonomy_featured_image" id="taxonomy_featured_image" value="">
+            <div id="image-container"></div>
+            <input class="button button-primary" id="select_taxonomy_image" type="button" value="Select/Upload Image" />
+        </div>
+        <?php
+        $this->_script();
+    }
+
+    /**
+     * HTML for edit taxonomy pages.
+     */
+    protected function _editFormFields( $term )
+    {
+        // Enqueue the media.
+        wp_enqueue_media();
+
+        $imageId = get_term_meta( $term->term_id, '_featured_image_id', true );
+        $imageUrl = wp_get_attachment_image_url( $imageId, 'medium' );
+        ?>
+
+        <tr class="form-field term-slug-wrap">
+            <th scope="row"><label for="slug">Featured Image</label></th>
+            <td>
+                <input type="hidden" name="taxonomy_featured_image" id="taxonomy_featured_image" value="">
+                <div id="image-container">
+                    <?php if ( $imageId ) : ?>
+                        <img src="<?php echo $imageUrl; ?>">
+                    <?php endif; ?>
+                </div>
+                <input class="button button-primary" id="select_taxonomy_image" type="button" value="Select/Upload Image" />
+            </td>
+        </tr>
+        <?php
+        $this->_script();
+    }
+
+    /**
+     * @param $termId
+     */
+    protected function _saveMeta( $termId )
+    {
+        if ( isset( $_POST['taxonomy_featured_image'] ) )
+        {
+            // Reference the image ID.
+            $imageId = $_POST['taxonomy_featured_image'];
+
+            // Set term meta.
+            update_term_meta( $termId, '_featured_image_id', $imageId );
+        }
+    }
+
     protected function addFormFields()
     {
+        // Loop through bindings.
+        foreach( $this->bindings as $binding )
+        {
+            // Hook new taxonomy menu page.
+            add_action( "{$binding->taxonomy}_add_form_fields", function() {
+                $this->_addFormFields();
+            });
+
+            // Hook edit taxonomy menu page.
+            add_action( "{$binding->taxonomy}_edit_form_fields", function( $term ) {
+                $this->_editFormFields( $term );
+            });
+        }
     }
 
     protected function saveFormFields()
     {
-
+        // Loop through bindings.
+        foreach( $this->bindings as $binding )
+        {
+            add_action( "create_{$binding->taxonomy}", function( $termId ) {
+                $this->_saveMeta( $termId );
+            });
+            add_action( "edited_{$binding->taxonomy}", function( $termId ) {
+                $this->_saveMeta( $termId );
+            });
+        }
     }
 
     /**
