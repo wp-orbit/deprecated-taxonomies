16622fa86cb903572fa08e946e9cadc06ed870ff
Marty Eason <martye@sideways8.com>
2017-02-19 17:53:32 -0500

    Add getPivotedPosts(), getPivotedParentPosts()

--- code/Pivoter/TaxonomyPivoter.php
+++ code/Pivoter/TaxonomyPivoter.php
@@ -168,4 +168,79 @@ class TaxonomyPivoter
             $this->pivotUsers();
         }
     }
+
+    /**
+     * Returns posts whose relationship exists through the presence of taxonomy terms.
+     * @param $postId int The post ID whose related posts we want.
+     * @param $taxonomy string The taxonomy through which relationships are established
+     * @param $postType string The post type of the related posts.
+     * @return \WP_Post[]
+     */
+    public static function getPivotedParentPosts( $postId, $taxonomy, $postType )
+    {
+        $args = [
+            'post_type' => $postType,
+            'tax_query' => [
+                [
+                    'taxonomy' => $taxonomy,
+                    'field'    => 'name',
+                    'terms'    => (string) $postId
+                ]
+            ],
+            'orderby'   => 'title',
+            'order'     => 'ASC',
+            'nopaging'  => true
+        ];
+
+        // Provide a filter for fetching pivotable posts.
+        $args = apply_filters( 'get_pivoted_post_args', $args, $postId, $taxonomy, $postType );
+
+        // Get WP Query.
+        $query = new \WP_Query( $args );
+
+        // No people.
+        if ( empty( $query->posts ) )
+        {
+            return [];
+        }
+
+        return $query->posts;
+    }
+
+    /**
+     * Returns an array of child pivoted posts.
+     * @param $postId
+     * @param $taxonomy
+     * @param $postType
+     * @return \WP_Post[]
+     */
+    public static function getPivotedPosts( $postId, $taxonomy, $postType )
+    {
+        $pivoter = new TaxonomyPivoter( $postId, $taxonomy );
+
+        // Get IDs.
+        $postIds = $pivoter->getIds();
+
+        $args = [
+            'post_type' => $postType,
+            'post__in'  => $postIds,
+            'orderby'   => 'title',
+            'nopaging'  => true
+        ];
+
+        // Prepare WordPress query.
+        $query = new \WP_Query( $args );
+
+        // Provide a filter for fetching pivotable posts.
+        $args = apply_filters( 'get_pivoted_direct_post_args', $args, $postId, $taxonomy, $postType );
+
+        // No people.
+        if ( empty( $query->posts ) )
+        {
+            return [];
+        }
+
+        return $query->posts;
+    }
+
 }
